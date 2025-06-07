4f66a0d39898655ec59d5f22ac4da584d8fbc705
Dulith D <as2020965@sci.sjp.ac.lk>
2025-06-07 22:34:18 +0530

    add the new changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask on change the your code now 2"
+    return "Hello from Flask on push to corretly configured branch!"
 
 if __name__ == '__main__':
     app.run()
